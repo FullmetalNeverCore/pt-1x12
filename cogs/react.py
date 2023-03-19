c383c85a8d5a49dc64f7b9fa2f57ec472f1a180b
0xNCORE <60969891+FullmetalNeverCore@users.noreply.github.com>
2023-03-19 16:58:09 +0200

    Add files via upload

--- cogs/react.py
+++ cogs/react.py
@@ -8,6 +8,7 @@ from discord.utils import get
 class React(commands.Cog):
     def __init__(self,bot):
         self.sys = bot 
+        self.union = self.sys.get_guild(int('1085209458633887885'))
         self.channel = 1 
 
     @commands.Cog.listener()
@@ -18,27 +19,28 @@ class React(commands.Cog):
         print("## COG: Отключение...")
 
     @commands.command()
+    @commands.is_owner()
     async def set_channel(self,ctx):
-        if ctx.author.id == 224486385992728576:
             print('## COG: Смена канала.')
             self.channel = ctx.message.content.split()[1]
     
     @commands.command()
+    @commands.is_owner()
     async def print_channel(self,ctx):
         print(self.channel)    
     
     @commands.command()
+    @commands.is_owner()
     async def test_grt_msg(self,ctx):
-        if ctx.author.id == 224486385992728576:
-            union = self.sys.get_guild(int('1085209458633887885'))
-            target_channel = get(union.text_channels,id=int(self.channel))
+            target_channel = get(self.union.text_channels,id=int(self.channel))
             embed = discord.Embed(title=f'## Авторизация',color=0xff0000)
             embed.add_field(name=f'''
 Проект «Красный Рассвет» готов к защите своих интересов от внешних угроз. Нам нужны сотрудники КГБ и милиции, обученные граждане, новые каналы и научные центры. 
 Мы должны быть лидерами в мире Дискорда. 
-Участвуйте в укреплении нашей обороны и безопасности!
+Участвуйте в укреплении нашей безопасности!
             ''',value= '\u200b')
-            embed.set_footer(text='Ген.сек. ЦК КПСС Юрьевич Назар Бесько')
+            embed.set_footer(text='Ген.сек. ЦК КПСС Юрьевич Назар')
+            embed.set_image(url='https://cdn.discordapp.com/attachments/533392531493355569/1086407593435602994/dahevi44__Create_a_digital_artwork_that_combines_elements_of_US_04683c5e-19ea-410c-81df-846433c687aa.png')
             moji = await target_channel.send(embed=embed)
             await moji.add_reaction('☑️')
 
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from discord.utils import get
 intents = discord.Intents.all()
 intents.members = True 
 intents.messages = True
-sys = commands.Bot(command_prefix=">",intents=intents)
+owner_id=224486385992728576
+sys = commands.Bot(command_prefix=">",owner_id=owner_id,intents=intents)
 
 
 @sys.event
@@ -14,18 +15,34 @@ async def on_ready():
     print(f'Система PT-1X12 готова.')
 
 @sys.command()
+@commands.is_owner()
 async def load(ctx,extension):
     print(ctx.author.id)
-    if ctx.author.id == 224486385992728576:
-        await sys.load_extension(f'cogs.{extension}')
-        await ctx.send(f'Модуль {extension} загружен.')
+    sys.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Модуль {extension} загружен.')
 
 
 @sys.command()
+@commands.is_owner()
 async def unload(ctx,extension):
-    if ctx.author.id == 224486385992728576:
-        await sys.load_extension(f'cogs.{extension}')
-        await ctx.send(f'Модуль {extension} отключен.')
+    sys.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Модуль {extension} отключен.')
+
+@sys.command()
+@commands.is_owner()
+async def owner(ctx):
+    print(ctx)
+    user = get(sys.get_all_members(), id=owner_id)
+    print(dir(user))
+    print(f'Owner - {owner_id}')
+    await ctx.send(f'Администратор подтвержден - {user.name}')
+
+@owner.error 
+async def owner_error(ctx,error):
+       print(error)
+       user = get(sys.get_all_members(), id=owner_id)
+       await ctx.send(f'Вы не являетесь администратором.Администратор - {user.name}') 
+
 
 @sys.event
 async def on_raw_reaction_add(payload):
@@ -40,4 +57,4 @@ async def on_raw_reaction_add(payload):
                 if payload.emoji.name == '☑️': await payload.member.add_roles(roles[0])
 
 
-sys.run('..')
+sys.run('MTA4NTk5MzY5NDU3NTk5Njk1OA.G-eMnF.94R5zXT_4jdxrEOKm04aLDN3Uo3yM2tUtI5bxc')
